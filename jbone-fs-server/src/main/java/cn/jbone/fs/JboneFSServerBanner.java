e1e88044a1e212aa41419b88e5143ae9fefbef3a
马军伟 <majunwei@weidian.com>
2019-11-16 08:05:48 +0800

    项目拆分

---
+++ jbone-fs-server/src/main/java/cn/jbone/fs/JboneFSServerBanner.java
@@ -0,0 +1,12 @@
+package cn.jbone.fs;
+
+
+import cn.jbone.banner.AbastractJboneBanner;
+
+public class JboneFSServerBanner extends AbastractJboneBanner {
+
+    @Override
+    protected String getTitle() {
+        return "Jbone File System Server";
+    }
+}
